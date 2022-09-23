78386f6d845a5a7b77f0aefaf2d06315bac781d6
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2022-09-22 19:10:20 -0500

    Added code for local storage of user score

--- script.js
+++ script.js
@@ -12,9 +12,13 @@ var score = 0;
 var quizDone = false;
 var questions = ["question1", "question2"];
 var answers = ["answer1", "answer2"];
-
+var user = {
+    intials: "",
+    score: ""
+};
 var buttonChoices = [["A", "answer1", "C", "D"], ["E", "F", "answer2", "H"]];
 var timerCount = 29;
+var verified = false;
 
 function startQuiz() {
     timerEl.textContent = timerCount + 1;
@@ -59,6 +63,23 @@ function endOfQuiz() {
     quizDone = true;
     percentRight = score / questions.length * 100;
     alert("Quiz is over. You got " + percentRight + "% of the questions right.");
+    var saveScore = confirm("Would you like to save your score?");
+    if (saveScore) {
+        while (!verified) {
+            userInt = prompt("Enter your intials (max 3 letters):");
+            
+            if(userInt.length < 4) {
+                user.intials = userInt;
+                user.score = score;
+                localStorage.setItem("user", JSON.stringify(user));
+                alert("Your score was saved");
+                verified = true;
+            }
+            else {
+                alert("Too many letters. Try again.");
+            }
+         }
+    }
 }
 
 function answerClick(event) {
@@ -96,3 +117,19 @@ function answerClick(event) {
 
 startButton.addEventListener("click", startQuiz);
 questionEl.addEventListener("click", answerClick);
+
+// signUpButton.addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     // create user object from submission
+//     var user = {
+//         firstName: firstNameInput.value.trim(),
+//         lastName: lastNameInput.value.trim(),
+//         email: emailInput.value.trim(),
+//         password: passwordInput.value.trim()
+//     };
+
+//     // set new submission to local storage 
+//     localStorage.setItem("user", JSON.stringify(user));
+
+// });
